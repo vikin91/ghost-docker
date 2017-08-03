14612e53cc5e1fb381e2c3e80db15d24c49b47a4
vikin91 <piotr@linux.pl>
2017-08-03 21:38:15 +0200

    Fix image migration

--- migrate-images.sh
+++ migrate-images.sh
@@ -3,11 +3,13 @@
 source "lib.sh"
 
 GHOST_CONTENT_SRC="/var/www/ghost/content"
+GHOST_CONTENT_DEST="/var/lib/ghost/content"
 
-IMAGES="$(find ${GHOST_CONTENT_SRC}/images/)"
+IMAGES="$(find ${GHOST_CONTENT_SRC}/images/ -maxdepth 1 ! -path ${GHOST_CONTENT_SRC}/images/ )"
 
-for E in "${IMAGES[@]}"; do
-	"${DOCKER}" cp "${E}" ghost:/var/lib/ghost/content/images/
+for IMAGE in ${IMAGES}; do
+	echo "Copying ${IMAGE}"
+	"${DOCKER}" cp "${IMAGE}" ghost:"${GHOST_CONTENT_DEST}"/images/
 done
 
 
