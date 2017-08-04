0546e0a4893f84f6a9de7229087d2730209b7b22
vikin91 <piotr@linux.pl>
2017-08-04 08:28:44 +0200

    Make update work when container is stopped

--- update.sh
+++ update.sh
@@ -8,8 +8,8 @@ IMAGE_TO_REMOVE="$("${DOCKER}" images --filter=reference='ghost:latest' -q)"
 # TODO: check if update is required
 
 echo "Stopping ghost"
-"${DOCKER_COMPOSE}" down
-"${DOCKER}" stop "${IMAGE_TO_REMOVE}"
+"${DOCKER_COMPOSE}" down || echo "No need to stop. Continuing"
+"${DOCKER}" stop "${IMAGE_TO_REMOVE}" || echo "No need to stop. Continuing"
 echo "Removing cached ghost image"
 "${DOCKER}" rmi "${IMAGE_TO_REMOVE}"
 
