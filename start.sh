0ed1a473aeaf0d56449c5ebe995a497233e0c6ac
vikin9 <piotr@linux.pl>
2018-04-19 20:32:57 +0200

    Harden update and start

--- start.sh
+++ start.sh
@@ -9,8 +9,8 @@ echo "Starting ghost"
 
 echo "Setting ghost URL to ${PAGE_URL}"
 
-"${DOCKER_COMPOSE}" exec blog ghost config url "${PAGE_URL}"
+"${DOCKER_COMPOSE}" exec --user node blog ghost config url "${PAGE_URL}"
 
-echo "Restarting ghost"
+echo "Restarting ghost so that URL setting goes live"
 
 "${DOCKER_COMPOSE}" restart blog
